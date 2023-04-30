af8ecc9eafd757103cd3bba95ecb7d3b77eb47ad
vladtka <vladtka05@gmail.com>
2023-04-30 22:46:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,3 +9,4 @@ bot = commands.Bot(command_prefix="*", intents=disnake.Intents.all())
 async def test(interaction: disnake.AppCmdInter):
 	await interaction.send("комманда выполнена!RTMC")
 
+bot.run("MTA5NzE3NjQzOTI3NjcyMDIzOA.GIFUtZ.P6hH4DEZZ1HjSeFVmJnQZHTl2BZ3KdJhj9VrqE")
